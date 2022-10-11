efe60f72400ec84410362faf7b0a825a32b5d545
Hades <phamvanlinh.hdes.1711@gmail.com>
2022-10-11 22:46:40 +0700

    bai tap dau tien

---
+++ BaiTap1/bai1.rb
@@ -0,0 +1,12 @@
+puts "Xin chào: Phạm Văn Linh - 1911505310133"
+
+puts "Nhâp vào 1 số: "
+num = gets
+puts "Số vừa nhập là: #{num}"
+
+puts "Nhập vào 2 số để so sánh: "
+print "Số thứ nhất: "
+a = gets
+print "Số thứ hai: "
+b = gets
+puts"Số lớn hơn là: #{a > b ? a : b}"
