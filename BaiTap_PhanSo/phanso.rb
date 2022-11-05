ef0c2751b47b99612f85c1987be3c79f01b805ea
Hades <phamvanlinh.hdes.1711@gmail.com>
2022-11-05 10:35:42 +0700

    helle

--- BaiTap_PhanSo/phanso.rb
+++ BaiTap_PhanSo/phanso.rb
@@ -1,70 +1,63 @@
-class Tinhtoan
-
-    attr_accessor :ts_133,:ms_133
-
-    def initialize(ts_133,ms_133)
-
-        @ts_133 = ts_133
-
-        @ms_133 = ms_133    
-
+class Tinhtoan 
+    attr_accessor :ps1,:ps2
+    def initialize(ps1,ps2)
+        @ps1 = ps1
+        @ps2 = ps2     
     end
-
     def Cong
-
-        puts "Tổng: #{(ts_133.t*ms_133.m+ms_133.t*ts_133.m)*0.1/(ts_133.m*ms_133.m)}"
-
+        puts "Tổng: #{(ps1.t*ps2.m+ps2.t*ps1.m)}/#{(ps1.m*ps2.m)}"
     end
-
     def Tru
-
-        puts "Hiệu #{ts_133.t*ms_133.m-ms_133.t*ts_133.m)*0.1/(ts_133.m*ms_133.m)}"
-
+        puts "Hiệu #{(ps1.t*ps2.m-ps2.t*ps1.m)}/#{(ps1.m*ps2.m)}" 
     end
-
     def Tich
-
-        puts "Tích: #{(ts_133.t*ms_133.t)*0.1/(ts_133.m*ms_133.m)}"
-
+        puts "Tích: #{(ps1.t*ps2.t)}/#{(ps1.m*ps2.m)}" 
     end
-
     def Thuong
-
-        puts "Thương: #{(ts_133.t*ms_133.m)*0.1/(ts_133.m*ms_133.t)}"
-
+        puts "Thương: #{(ps1.t*ps2.m)}/#{(ps1.m*ps2.t)}" 
     end
-
 end
 class Phanso
-
-    attr_accessor :t_133, :m_133
-
-    def initialize(t_133,m_133)
-
-        @t_133= t_133
-
-        @m_133 = m_133
-
+    attr_accessor :t, :m
+    def initialize(t,m)
+        @t= t
+        @m = m
     end
-
-    def ps
-
-        puts "Phân só"
-
-    end
-
 end
-
+class Phanso2
+    attr_accessor :t,:m
+    def initialize(t,m)
+        self.t=t
+        self.m = m
+    end
+    def +(other)
+        Phanso.new(((self.t*other.m) + (other.t*self.m)), self.m*other.m)
+    end
+    def -(other)
+        Phanso.new(self.t*other.m - other.t*self.m, self.m*other.m)
+    end
+    def *(other)
+        Phanso.new(self.t * other.t, self.m*other.m)
+    end
+    def /(other)
+        Phanso.new(self.t * other.m, self.m*other.t)
+    end
+end
 ps1 = Phanso.new(1,2)
-
 ps2 = Phanso.new(1,2)
-
-tt = Tinhtoan.new(ts_133,ms_133)
-
+tt = Tinhtoan.new(ps1,ps2)
 tt.Cong
-
 tt.Tru
-
 tt.Tich
-
 tt.Thuong
+puts "-----------------------------------"
+ps3 = Phanso2.new(1,2)
+ps4 = Phanso2.new(1,2)
+cong = ps3+ps4
+puts "Cộng: #{cong.t}/#{cong.m}"
+hieu = ps3 - ps4
+puts "Hiệu: #{cong.t}/#{cong.m}"
+tich = ps3 * ps4
+puts "Tích: #{cong.t}/#{cong.m}"
+thuong = ps3 / ps4
+puts "Thương: #{cong.t}/#{cong.m}"
